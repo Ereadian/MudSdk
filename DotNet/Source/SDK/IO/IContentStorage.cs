1d0ea01f991780f70f7adfbedde80db4b9246f89
ereadian <ereadian@gmail.com>
2017-03-15 20:47:59 -0700

    initial storage isolation

---
+++ DotNet/Source/SDK/IO/IContentStorage.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ereadian.MudSdk.Sdk.IO
+{
+    public interface IContentStorage
+    {
+    }
+}
---
+++ DotNet/Source/SDK/IO/ProfileFileStorage.cs
@@ -0,0 +1,12 @@
+﻿namespace Ereadian.MudSdk.Sdk.IO
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class ProfileFileStorage : IProfileStorage
+    {
+    }
+}
